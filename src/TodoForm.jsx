14b83d240f84704a402d3275a23206df1309a893
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-16 11:41:51 +0300

    lisätty fieldset ja legend elementit formiin

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,13 +1,16 @@
 const TodoForm = ({ handleChange, addTodo, formState }) => {
     return (
         <form onSubmit={addTodo}>
-            <label htmlFor="desc">
-                Description: <input type="text" name="desc" id="desc" placeholder="Description" onChange={handleChange} value={formState.desc} />
-            </label>
-            <label htmlFor="date">
-                Date: <input type="date" name="date" id="date" onChange={handleChange} value={formState.date} />
-            </label>
-            <button type="submit">Add</button>
+            <fieldset>
+                <legend>Add todo</legend>
+                <label htmlFor="desc">
+                    Description: <input type="text" name="desc" id="desc" placeholder="Description" onChange={handleChange} value={formState.desc} />
+                </label>
+                <label htmlFor="date">
+                    Date: <input type="date" name="date" id="date" onChange={handleChange} value={formState.date} />
+                </label>
+                <button type="submit">Add</button>
+            </fieldset>
         </form>
     )
 }
