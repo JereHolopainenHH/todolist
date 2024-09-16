1ecf81b5f0d4fc25ae419d8ed61f38c7c9d7694a
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-16 10:07:40 +0300

    Muutettu handleChange ja addTodo -funktioita
    handleChange hoitaa desc- ja date-input elementtien muuttamisen
    
    addTodo tarkistaa, että molemmat desc ja date on annettu ja lisää uuden todo-alkion todos-listaan, sekä resetoi formStaten

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -10,26 +10,41 @@ const TodoList = () => {
     const [todos, setTodos] = useState([]);
 
     // control the input value
-    const handleChange = (event) => setDesc(event.target.value);
+    const handleChange = (event) => {
+        // Destructure the name and value from the event target (the input element)
+        const { name, value } = event.target;
+        // Update the state based on the input field that triggered the event 
+        setFormState(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
     // add a new todo-item to the todos-list
     const addTodo = () => {
         // validate that description value exists, alert if not. 
-        if (!desc) return alert("Can't add empty items to the list!");
-        // add desc to list
-        setTodos([...todos, desc]);
-        // clear input
-        setDesc("");
+        if (!formState.desc || !formState.date) return alert("Both Description and Date required!");
+
+        // create new todo-object with the formState-data
+        const todo = {
+            desc: formState.desc,
+            date: formState.date
+        }
+        // add new todo-item to todos-list
+        setTodos(prevState => [...prevState, todo]);
+        // clear inputs
+        setFormState({ desc: "", date: "" });
     };
 
-    // delete item from the todoslist
+    // delete item from the todos-list
     const deleteTodo = (indexToRemove) => {
         setTodos([...todos].filter((todo, index) => index !== indexToRemove));
     }
 
     return (
         <>
-            <input type="text" placeholder="Description" onChange={handleChange} value={desc} />
+            <input type="text" name="desc" placeholder="Description" onChange={handleChange} value={formState.desc} />
+            <input type="date" name="date" onChange={handleChange} value={formState.date} />
             <button onClick={addTodo}>Add</button>
             <TodoTable todos={todos} deleteTodo={deleteTodo} />
         </>
