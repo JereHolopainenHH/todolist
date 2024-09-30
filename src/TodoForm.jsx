0be4b22949846b4f62908e717723815c059ae8ec
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-30 09:12:14 +0300

    Lisätty ag-grid

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,15 +1,19 @@
-const TodoForm = ({ handleChange, addTodo, formState }) => {
+const TodoForm = ({ handleChange, addTodo, handleDelete, formState }) => {
     return (
-        <form onSubmit={addTodo}>
+        <form>
             <fieldset>
                 <legend>Add todo</legend>
                 <label htmlFor="desc">
                     Description: <input type="text" name="desc" id="desc" placeholder="Description" onChange={handleChange} value={formState.desc} />
                 </label>
+                <label htmlFor="priority">
+                    Priority: <input type="text" name="priority" id="priority" placeholder="Priority" onChange={handleChange} value={formState.priority} />
+                </label>
                 <label htmlFor="date">
                     Date: <input type="date" name="date" id="date" onChange={handleChange} value={formState.date} />
                 </label>
-                <button type="submit">Add</button>
+                <button onClick={addTodo}>Add</button>
+                <button onClick={handleDelete}>Delete</button>
             </fieldset>
         </form>
     )
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
-import TodoTable from "./TodoTable";
+// import TodoTable from "./TodoTable";
 import TodoForm from "./TodoForm";
+import { AgGridReact } from "ag-grid-react";
+import { useRef, useState } from 'react'
+
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css"; // Material Design theme
 
 const TodoList = () => {
     // get current date that can be used as a default value for the input date element. 
@@ -10,9 +15,26 @@ const TodoList = () => {
     }
 
     // states
-    const initialFormState = { desc: "", date: getCurrentDate() };
+    const initialFormState = { desc: "", priority: "", date: getCurrentDate() };
     const [formState, setFormState] = useState(initialFormState);
     const [todos, setTodos] = useState([]);
+    /* const [columnDefs, setColumnDefs] = useState([
+        { field: 'desc' },
+        { field: 'priority' },
+        { field: 'date' }
+    ]); */
+    const columns = [
+        { field: "desc", sortable: true, filter: true },
+        {
+            field: "priority", sortable: true, filter: true,
+            cellStyle: params => params.value === "High" || params.value === "high" ? { color: 'red' } : { color: 'black' }
+        },
+        { field: "date", sortable: true, filter: true }
+    ];
+
+    // refs
+    const gridRef = useRef();
+
 
     // control the input value
     const handleChange = (event) => {
@@ -31,11 +53,12 @@ const TodoList = () => {
         event.preventDefault();
 
         // validate that description and date values exist and are valid, alert if not. 
-        if (!formState.desc || !formState.date) return alert("Invalid description or date! Make sure that both fields are filled.");
+        if (!formState.desc || !formState.date || !formState.priority) return alert("Invalid form values! Make sure that all fields are filled.");
 
         // create new todo-object with the formState-data
         const todo = {
             desc: formState.desc,
+            priority: formState.priority,
             date: formState.date
         }
 
@@ -46,14 +69,29 @@ const TodoList = () => {
     };
 
     // delete item from the todos-list
-    const deleteTodo = (indexToRemove) => {
-        setTodos(todos.filter((todo, index) => index !== indexToRemove));
+    const handleDelete = (event) => {
+        event.preventDefault();
+        if (gridRef.current.getSelectedNodes().length > 0) {
+            setTodos(todos.filter((todo, index) =>
+                index != gridRef.current.getSelectedNodes()[0].id))
+        }
+        else {
+            alert('Select a row first!');
+        }
     }
 
     return (
         <>
-            <TodoForm handleChange={handleChange} addTodo={addTodo} formState={formState} />
-            <TodoTable todos={todos} deleteTodo={deleteTodo} />
+            <TodoForm handleChange={handleChange} addTodo={addTodo} handleDelete={handleDelete} formState={formState} />
+            <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowData={todos}
+                    columnDefs={columns}
+                    rowSelection="single"
+                />
+            </div>
         </>
     );
 }
